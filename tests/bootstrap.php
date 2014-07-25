954d2e323d7db9060eb0ceb80118af9de6fb7bf8
Tim Wagner <t.wagner@techdivision.com>
2014-07-25 14:19:59 +0200

    Add temporary files created by PHPUnit tests to gitignore file

--- tests/bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$loader = require '${php-target.dir}/vendor/autoload.php';
-$loader->add('TechDivision\\MessageQueueProtocol\\', '${php-target.dir}/vendor/techdivision/messagequeueprotocol/src');
